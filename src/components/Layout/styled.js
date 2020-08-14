a664e2c04c4b251933f5e063e90335db7c54264d
Junior <jjalves96@gmail.com>
2020-08-14 16:05:39 -0300

    fix: styled into Layout component

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -2,14 +2,12 @@ import styled from 'styled-components';
 
 export const LayoutWrapper = styled.section`
   display: flex;
-  flex-direction: column;
-  background-color: var(--background-layout-main);
 `;
 
 export const LayoutMain = styled.main`
-  /* Menos o altura do Profile */
+  background: var(--background-layout-main);
   min-height: 100vh;
-  max-width: 1200px;
   font-size: 1.6rem;
-  margin: 0 auto;
+  padding-left: 28rem;
+  width: 100%;
 `;
