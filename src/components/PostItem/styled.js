3e2760c1ba1a21af325f0549a59e2d93c099e9fc
Junior <jjalves96@gmail.com>
2020-08-14 17:21:38 -0300

    feat: create PostItem component

---
+++ src/components/PostItem/styled.js
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+export const PostItemLink = styled(Link)`
+  color: #ddd;
+  display: flex;
+  text-decoration: none;
+  transition: color 0.3s;
+
+  &:hover {
+    color: #888;
+  }
+`;
+
+export const PostItemWrapper = styled.section`
+  align-items: center;
+  border-bottom: 1px solid #fff;
+  display: flex;
+  padding: 2rem 3rem;
+  width: 100%;
+`;
+
+export const PostItemTag = styled.div`
+  align-items: center;
+  background: ${props => (props.background ? props.background : '#7159c1')};
+  border-radius: 50%;
+  color: #fff;
+  display: flex;
+  font-size: 2rem;
+  font-weight: 700;
+  justify-content: center;
+  min-height: 90px;
+  min-width: 90px;
+  text-transform: uppercase;
+`;
+
+export const PostItemInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 1.5rem;
+`;
+
+export const PostItemDate = styled.time`
+  font-size: 1.2rem;
+`;
+
+export const PostItemTitle = styled.h1`
+  font-size: 2.4rem;
+  font-weight: 700;
+  margin: 0.6rem 0 1rem;
+`;
+
+export const PostItemDescription = styled.p`
+  font-size: 1.6rem;
+  font-weight: 300;
+  line-height: 1.2;
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 import Layout from '../components/Layout';
+import PostItem from '../components/PostItem';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
@@ -10,6 +11,15 @@ const IndexPage = () => (
     <Helmet>
       <title>Home</title>
     </Helmet>
+    <PostItem
+      slug="/about/"
+      background="red"
+      category="Misc"
+      date="14 de Agosto"
+      timeToRead="3"
+      title="Title"
+      description="Description"
+    />
   </Layout>
 );
 
