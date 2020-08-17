920cb06de755201307442e91bfcca7c231a5395f
Junior <jjalves96@gmail.com>
2020-08-17 16:39:21 -0300

    feat: robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,6 +67,8 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // XML, para rodar, gatsby build && gatsby-server
+    `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
