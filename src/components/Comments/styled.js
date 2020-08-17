e649060c4066c68dd18dfe94b17187c0c51667f2
Junior <jjalves96@gmail.com>
2020-08-17 15:53:44 -0300

    feat: Comments component

---
+++ src/components/Comments/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const CommentsWrapper = styled.section`
+  margin: auto;
+  max-width: 70rem;
+  padding: 3rem 6.4rem 3rem;
+
+  iframe[src*='ads-iframe'] {
+    display: none;
+  }
+  #disqus_thread {
+    a {
+      color: var(--color-link-primary) !important;
+    }
+  }
+`;
+
+export const CommnentsTitle = styled.h2`
+  color: var(--post-color);
+  font-size: 2.4rem;
+  font-weight: 700;
+  padding-bottom: 0;
+`;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 import RecommendedPosts from '../components/RecommendedPosts';
+import Comments from '../components/Comments';
 import SEO from '../components/seo';
 
 import * as S from '../components/Post/styled';
@@ -29,6 +30,7 @@ const BlogPost = ({ data, pageContext }) => {
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </S.MainContent>
       <RecommendedPosts next={next} previous={previous} />
+      <Comments url={post.fields.slug} title={post.frontmatter.title} />
     </Layout>
   );
 };
