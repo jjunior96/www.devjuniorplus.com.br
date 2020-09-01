f31266a34de543464cb086f233a0cc04a67575f5
Junior <jjalves96@gmail.com>
2020-09-01 15:25:43 -0300

    feat: new site visual

--- src/styles/global.js
+++ src/styles/global.js
@@ -60,7 +60,8 @@ const GlobalStyles = createGlobalStyle`
     --color-profile-hover: #767b91;
     
     /* line PostItem */
-    --color-line-postitem: #4E5166;
+    /* --color-line-postitem: #4E5166; */
+    --color-line-postitem: #4E51664D;
 
     /* Menu Links */
     --color-link-primary: #e1e5ee;
