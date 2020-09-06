d6f9b4414e9730a5f912f59bbe4b31f109f90ca1
Junior <jjalves96@gmail.com>
2020-09-06 09:13:11 -0300

    chore: add Update on ServiceWorker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,16 @@
+/* eslint-disable no-undef */
 import 'lazysizes';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SwUpdater from './src/components/SwUpdater';
 
-// require('prismjs/themes/prism-tomorrow.css');
 require('prismjs/themes/prism-dracula.css');
+
+function onServiceWorkerUpdateReady() {
+  const root = document.createElement('div');
+  document.body.appendChild(root);
+
+  ReactDOM.render(<SwUpdater />, root);
+}
+
+export { onServiceWorkerUpdateReady };
