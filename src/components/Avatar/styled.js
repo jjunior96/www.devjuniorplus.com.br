14048dddd2b08ca670b04ed826cf9e170867443b
Junior <jjalves96@gmail.com>
2020-10-18 08:53:14 -0300

    chore: add new styles for Avatar

--- src/components/Avatar/styled.js
+++ src/components/Avatar/styled.js
@@ -4,7 +4,7 @@ import Img from 'gatsby-image';
 
 export const AvatarWrapper = styled(Img)`
   border-radius: 20px;
-  height: 11rem;
+  height: auto;
   margin: auto;
   margin-top: 2rem;
   width: 14rem;
